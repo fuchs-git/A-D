76cb4ab6e25da552c5d28d554d963dc35608db13
fuchs <pasvoss@gmail.com>
2025-10-02 15:57:17 +0200

    Aufgabe 5 - Im Wagon wird appended

---
+++ 01_Aufgaben/linkedList_05_appand.py
@@ -0,0 +1,100 @@
+from typing import Any
+
+from LinkedList.liste import Wagon
+
+
+class Liste:
+
+    def __init__(self):
+        self.first = None
+
+    def repr_iterativ(self):
+        schaffner = self.first
+        values = ''
+        while schaffner is not None:
+            if schaffner.value is not None:
+                if values:
+                    values += ', '
+                values += repr(schaffner.value)
+            schaffner = schaffner.next
+        return f'[{values}]' if values else '[]'
+
+    def __repr__(self):
+        if self.first is None:
+            return '[]'
+        return '[' + repr(self.first)
+
+    def len_iterativ(self):
+        if self.first is None:
+            return 0
+        else:
+            schaffner = self.first
+            length = 1
+            while schaffner.next is not None:
+                length += 1
+                schaffner = schaffner.next
+            return length
+
+    # ==================================================================
+    # Länge wird im Wagon ermittelt
+    # ==================================================================
+    def __len__(self) -> int:
+        if self.first is None:
+            return 0
+        return len(self.first)
+
+    def append(self, value: Any):
+        neuer_wagon = Wagon(value)
+        if self.first is None:
+            self.first = neuer_wagon
+            # print(f'Neuer Wagon mit Value {value}')
+        else:
+            schaffner = self.first
+            while schaffner.next is not None:
+                schaffner = schaffner.next
+            schaffner.next = neuer_wagon
+            # print(f'Weiterer Wagon mit Value {value}')
+
+    def append2(self, value: Any):
+        if self.first is None:
+            self.first = Wagon(value)
+            return
+        self.first.append(value)
+
+
+class Wagon:
+    # ==================================================================
+    # Rückwärts ermittelt
+    # ==================================================================
+    def __len__(self):
+        if self.next is None:
+            return 1
+        return len(self.next) + 1
+
+    def __init__(self, value: Any):
+        self.next = None
+        self.value = value
+
+    # ==================================================================
+    # String Ausgabe rückwärts ermittelt
+    # ==================================================================
+    def __repr__(self):
+        if self.next is None:
+            return repr(self.value) + ']'
+        return f'{repr(self.value)}, ' + repr(self.next)
+
+    def append(self, value):
+        if self.next is None:
+            self.next = Wagon(value)
+        else:
+            self.next.append(value)
+
+
+lst = Liste()
+lst2 = Liste()
+[lst.append(x) for x in range(3)]
+[lst2.append2(x) for x in range(3)]
+lst.append('drei')
+lst2.append2('drei')
+print(lst)
+print(lst2)
